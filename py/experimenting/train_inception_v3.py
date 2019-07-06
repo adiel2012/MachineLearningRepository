44db088e26c64276abbc1443e55b9efa6620fd39
adiel2012 <emileydis2012>
2019-07-06 01:07:15 -0400

    Data Augmerntation

--- py/experimenting/train_inception_v3.py
+++ py/experimenting/train_inception_v3.py
@@ -12,6 +12,6 @@ reshapeTo = (75, 75)
 #https://keras.io/applications/#inceptionv3
 
 model = keras.applications.inception_v3.InceptionV3(include_top=True, weights=None, input_tensor=None, input_shape=(75, 75, 3), pooling=None, classes=nb_classes)
-train_classification_model(model, 'cifar10', batch_size, nb_classes, nb_epoch, img_rows, img_cols, img_channels, data_augmentation, reshapeTo = reshapeTo)
+train_classification_model(model, 'cifar10', batch_size, nb_classes, nb_epoch, img_rows, img_cols, img_channels, data_augmentation = True, reshapeTo = reshapeTo,  featurewise_center=True, samplewise_center=True,  featurewise_std_normalization=True,  samplewise_std_normalization=True)
 
 print('FIN')
