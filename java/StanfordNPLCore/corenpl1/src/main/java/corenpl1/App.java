1562aced104bb17c6a855c5a276e2f0346026f9d
adiel2012 <emileydis2012>
2019-07-07 04:43:42 -0400

    stanford npl

--- java/StanfordNPLCore/corenpl1/src/main/java/corenpl1/App.java
+++ java/StanfordNPLCore/corenpl1/src/main/java/corenpl1/App.java
@@ -1,5 +1,14 @@
 package corenpl1;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,57 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // build in this way   mvn clean compile assembly:single
+        //   java -jar target\acmartifactid-1.0-SNAPSHOT-jar-with-dependencies.jar
+        init();
+        String[] sentences = new String[]{"It is a Beautiful Day", "this is the worst day of my life"};
+
+        for(String str : sentences)
+        {
+            System.out.println(str + "----->" + String.valueOf(findSentiment(str)) );
+        }
+
+        System.out.println( "--------------------" );
+    }
+
+
+
+    static StanfordCoreNLP pipeline;
+ 
+    public static void init() {
+
+         // set up pipeline properties
+        Properties props = new Properties();
+        // set the list of annotators to run
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
+        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
+        props.setProperty("coref.algorithm", "neural");
+        // build pipeline
+        pipeline = new StanfordCoreNLP(props);
+
+        //pipeline = new StanfordCoreNLP("nlp.properties");
+        System.out.println("End read pipeline");
+    }
+  
+    public static int findSentiment(String text) {
+ 
+        int mainSentiment = 0;
+        if (text != null && text.length() > 0) {
+            int longest = 0;
+            Annotation annotation = pipeline.process(text);
+            for (CoreMap sentence : annotation
+                    .get(CoreAnnotations.SentencesAnnotation.class)) {
+                Tree tree = sentence
+                        .get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+ 
+            }
+        }
+        return mainSentiment;
     }
 }
